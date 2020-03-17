7e1ea9c2539f3e0349cc3baac9a1d10a3a04ec77
Mark-Ball <markball111@gmail.com>
2020-03-17 14:25:49 +1100

    created http service and retrieved data

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from '../../models/Todo';
 
 @Component({
@@ -8,18 +8,25 @@ import { Todo } from '../../models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() emitChecked = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // key is the class to add, value is the conditional (true or false)
   setClasses() {
     return { 'is-complete': this.todo.completed };
   }
 
-  clickCheckbox() {
-    console.log('clicked');
+  // this only toggles the UI, not the actual data
+  onToggle(item) {
+    console.log('toggled');
+    item.completed = !item.completed
   }
 
+  onDelete(item) {
+    console.log('deleted');
+  }
 }
